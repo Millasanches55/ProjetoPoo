e8fdbed78502ac4d6dc91d9caec7268720da4d61
Millasanches55 <106831415+Millasanches55@users.noreply.github.com>
2025-05-06 15:54:11 -0300

    Create PdfExtractor.java

---
+++ ProjetoPoo/SimpleGeminiApp/src/java/service/PdfExtractor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+/**
+ *
+ * @author Fatec
+ */
+public class PdfExtractor {
+    
+}
