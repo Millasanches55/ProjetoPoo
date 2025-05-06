9e26606afbf56cbe91b130fda2c35d10ee18b752
Millasanches55 <106831415+Millasanches55@users.noreply.github.com>
2025-05-06 15:57:27 -0300

    Create ResumoService.java

---
+++ ProjetoPoo/SimpleGeminiApp/src/java/service/ResumoService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import com.google.gson.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+/**
+ *
+ * @author Fatec
+ */
+public class ResumoService {
+    
+}
