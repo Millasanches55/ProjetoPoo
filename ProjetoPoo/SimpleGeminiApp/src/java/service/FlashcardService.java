04005ff05fed04a888f6edd63dd2210143213cbf
Millasanches55 <106831415+Millasanches55@users.noreply.github.com>
2025-05-06 15:38:16 -0300

    Create FlashcardService.java

---
+++ ProjetoPoo/SimpleGeminiApp/src/java/service/FlashcardService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author Fatec
+ */
+
+import com.google.gson.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class FlashcardService {
+    
+}
