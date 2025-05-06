6a58836567900e849d15b98e7ba3de099582e4fe
Millasanches55 <106831415+Millasanches55@users.noreply.github.com>
2025-05-06 16:35:57 -0300

    Escondendo a chave api

--- ProjetoPoo/SimpleGeminiApp/src/java/controller/CorrecaoServlet.java
+++ ProjetoPoo/SimpleGeminiApp/src/java/controller/CorrecaoServlet.java
@@ -14,7 +14,7 @@ import java.nio.charset.StandardCharsets;
 
 
 public class CorrecaoServlet extends HttpServlet {
-    private static final String API_KEY = "AIzaSyA-xNbUAj18cLFGuowbMvPpAy88IXeE_os"; 
+    /*APIKEY*/ 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
